0cb9491a76db6288b91b51c0c6ded6cefff0c420
Alex Foster <A.Foster@Fourleaf.local>
2021-09-24 15:18:42 +0100

    remove li

--- pages/test-video.js
+++ pages/test-video.js
@@ -14,41 +14,11 @@ const query = `
   `;
 
 function TestVideoPage({ videoData }) {
-  // const [videosLoaded, setVideosLoaded] = React.useState(false);
-  // const [videoData, setVideoData] = React.useState(null);
-
-  // const token = process.env.NEXT_PUBLIC_contentful_access_token;
-
-  // React.useEffect(() => {
-  //   window
-  //     .fetch(
-  //       "https://graphql.contentful.com/content/v1/spaces/" +
-  //         process.env.NEXT_PUBLIC_contentful_space_id +
-  //         "/",
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Authorization: `Bearer ` + token,
-  //         },
-  //         body: JSON.stringify({ query }),
-  //       }
-  //     )
-  //     .then((response) => response.json())
-  //     .then(({ data, errors }) => {
-  //       if (errors) {
-  //         console.error(errors);
-  //       }
-  //       setVideoData(data.workoutVideosCollection.items);
-  //     })
-  //     .then(console.log(videoData[0]))
-  //     .then(setVideosLoaded(true));
-  // }, []);
 
   const videoArray = videoData.data.workoutVideosCollection.items;
 
   return (
-    <ul>
+    <div>
       {videoData !== null && (
         <>
           {console.log("video data: ", videoArray)}
@@ -64,7 +34,7 @@ function TestVideoPage({ videoData }) {
           })}
         </>
       )}
-    </ul>
+    </div>
   );
 }
 
