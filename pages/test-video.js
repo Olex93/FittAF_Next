4047ef34ee52bb2acaab647bf656181fb427a0f7
Alex Foster <A.Foster@Fourleaf.local>
2021-09-24 14:14:31 +0100

    test page

---
+++ pages/test-video.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const query = `
+{
+    workoutVideosCollection{ 
+      items {
+        title
+        videoFile {
+          url
+        }
+      }
+    }
+  }
+  `;
+
+function TestVideoPage({ data }) {
+  console.log(data);
+  const [imagesLoaded, setImagesLoaded] = React.useState(false);
+
+  const token = process.env.NEXT_PUBLIC_contentful_access_token
+
+  React.useEffect(() => {
+    window
+      .fetch(
+        "https://graphql.contentful.com/content/v1/spaces/" +
+          process.env.NEXT_PUBLIC_contentful_space_id +
+          "/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ` + token,
+          },
+          body: JSON.stringify({ query }),
+        }
+      )
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+        console.log(data);
+      })
+      .then(setImagesLoaded(true));
+  }, []);
+
+  return (
+    <ul>
+      <li>test</li>
+    </ul>
+  );
+}
+
+export default TestVideoPage;
